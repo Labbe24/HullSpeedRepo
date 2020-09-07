aa9cfe7517aaf44ad52aad62284da5c5bc9b194c
Lasse Andresen <lasseandresen.1996@hotmail.com>
2020-09-07 14:24:16 +0200

    Added Hullspeed.App solution

--- HullSpeed.App/MainWindow.xaml.cs
+++ HullSpeed.App/MainWindow.xaml.cs
@@ -20,9 +20,35 @@ namespace HullSpeed.App
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Sailboat boat1 = null;
         public MainWindow()
         {
             InitializeComponent();
+
+            boat1 = new Sailboat();
+
+            Loaded += new RoutedEventHandler(MainWindow_Loaded);
+
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            Keyboard.Focus(TxtName);
+        }
+
+        private void NameTextChanged(object sender, TextChangedEventArgs e)
+        {
+           boat1.Name = TxtName.Text;
+        }
+
+        private void LengthTextChanged(object sender, TextChangedEventArgs e)
+        {
+            boat1.Length = double.Parse(TxtLength.Text);
+        }
+
+        private void calcHullspeedBtn_Click(object sender, RoutedEventArgs e)
+        {
+            TxtHullspeed.Text = boat1.Hullspeed().ToString("F1");
         }
     }
 }
